3fd3e12e0160ce147af1aa2267333827c9c69219
pausm99 <pausm99@gmail.com>
2024-05-27 19:17:11 +0200

    created league router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express'
+import leagueRoutes from './routes/leagueRoutes';
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/leagues', leagueRoutes)
 
 const { PORT } = process.env;
 
