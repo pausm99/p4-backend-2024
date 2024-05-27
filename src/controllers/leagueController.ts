5bf6b85da3c8b958daa51d0e923c8f4791774f4a
pausm99 <pausm99@gmail.com>
2024-05-27 19:42:28 +0200

    use catchError error handler on function controller

--- src/controllers/leagueController.ts
+++ src/controllers/leagueController.ts
@@ -1,10 +1,11 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import db from "../db/db";
 import { send } from "../handlers/response";
+import { catchErrors } from "../handlers/errors";
 
 export class LeagueController {
-  getAllLeagues = async (req: Request, res: Response) => {
+  getAllLeagues = catchErrors(async (req: Request, res: Response, next: NextFunction) => {
     const leagues = await db.league.findMany();
     send(res).ok(leagues);
-  };
+  });
 }
