d2fda2a48103a299c09fc68800f1968d27856e7f
pausm99 <pausm99@gmail.com>
2024-05-28 09:23:59 +0200

    install and add cors package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import cors from 'cors';
 import express from 'express';
+
 import { defaultErrorHandler } from './handlers/errors';
 import routes from './routes/index';
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 
 app.use('/', routes)
@@ -11,7 +15,6 @@ app.use('/', routes)
 app.use(defaultErrorHandler);
 
 const { PORT } = process.env;
-
 app.listen(PORT || 3000, () => {
     console.log(`Server running on port ${PORT}`);
 })
