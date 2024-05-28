aa73be87e2df4c5f970c95d04a56605d65e2bb26
pausm99 <pausm99@gmail.com>
2024-05-28 09:20:34 +0200

    added team routes and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import leagueRoutes from "./leagueRoutes";
+import teamRoutes from "./teamRoutes";
 
 const router = express.Router()
 
 router.use('/leagues', leagueRoutes);
+router.use('/teams', teamRoutes)
 
 export default router;
---
+++ src/routes/teamRoutes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { TeamController } from "../controllers/teamController";
+
+const router = express.Router()
+
+const teamController = TeamController;
+
+// router.route('/').get(teamController.)
+
+export default router;
