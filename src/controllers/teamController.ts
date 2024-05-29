5c223bda6ab0241eb80f54c19507183f5db9d60d
pausm99 <pausm99@gmail.com>
2024-05-29 19:19:58 +0200

    added team delete and update functions

--- src/controllers/teamController.ts
+++ src/controllers/teamController.ts
@@ -8,11 +8,11 @@ const idParamSchema = z.object({
   id: z.coerce.number(),
 });
 
-const temBodySchema = z.object({
+const teamBodySchema = z.object({
   name: z.string().min(3).max(50),
   leagueId: z.coerce.number().min(1),
-  city: z.string().min(2).max(50)
-})
+  city: z.string().min(2).max(50),
+});
 
 export class TeamController {
   getAllTeams = catchErrors(async (req: Request, res: Response) => {
@@ -27,8 +27,24 @@ export class TeamController {
   });
 
   createTeam = catchErrors(async (req: Request, res: Response) => {
-    const data = temBodySchema.parse(req.body);
-    const addedTeam = await db.team.create({ data })
-    send(res).createOk(addedTeam)
+    const data = teamBodySchema.parse(req.body);
+    const addedTeam = await db.team.create({ data });
+    send(res).createOk(addedTeam);
+  });
+
+  updateTeam = catchErrors(async (req: Request, res: Response) => {
+    const { id: teamId } = idParamSchema.parse(req.params)
+    const data = teamBodySchema.parse(req.body);
+    const updatedTeam = await db.team.update({
+      where: { teamId: teamId },
+      data: data
+    })
+    return send(res).ok(updatedTeam)
+  });
+
+  deleteTeam = catchErrors(async (req: Request, res: Response) => {
+    const { id: teamId } = idParamSchema.parse(req.params);
+    const deletedTeam = await db.team.delete({ where: { teamId } });
+    send(res).ok(deletedTeam);
   });
 }
--- src/routes/teamRoutes.ts
+++ src/routes/teamRoutes.ts
@@ -8,5 +8,7 @@ const teamController = new TeamController();
 router.route('/').get(teamController.getAllTeams)
 router.route('/:id').get(teamController.getTeamById)
 router.route('/').post(teamController.createTeam)
+router.route('/:id').delete(teamController.deleteTeam)
+router.route('/:id').put(teamController.updateTeam)
 
 export default router;
